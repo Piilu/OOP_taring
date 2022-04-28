30155bb35ee11225677fb12eae871a5299739f59
Kaspar Tulit <piilrainer@gmail.com>
2022-04-28 12:33:14 +0300

    first commit

---
+++ piil_rainer_täring/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace piil_rainer_täring
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.jukuScore = new System.Windows.Forms.Label();
+            this.peeterScore = new System.Windows.Forms.Label();
+            this.lblJukuRand1 = new System.Windows.Forms.Label();
+            this.lblJukuRand2 = new System.Windows.Forms.Label();
+            this.lblPeeterRand2 = new System.Windows.Forms.Label();
+            this.lblPeeterRand1 = new System.Windows.Forms.Label();
+            this.playJuku = new System.Windows.Forms.Button();
+            this.playPeeter = new System.Windows.Forms.Button();
+            this.btnStartNew = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblTulemus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(33, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 53);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Esimene mängija Juku";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(278, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 53);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Teine mängija Peeter";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Green;
+            this.label3.Location = new System.Drawing.Point(29, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 49);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Punktid";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Green;
+            this.label4.Location = new System.Drawing.Point(274, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(154, 49);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Punktid";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // jukuScore
+            // 
+            this.jukuScore.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.jukuScore.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.jukuScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.jukuScore.ForeColor = System.Drawing.Color.Green;
+            this.jukuScore.Location = new System.Drawing.Point(37, 153);
+            this.jukuScore.Name = "jukuScore";
+            this.jukuScore.Size = new System.Drawing.Size(146, 34);
+            this.jukuScore.TabIndex = 4;
+            this.jukuScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // peeterScore
+            // 
+            this.peeterScore.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.peeterScore.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.peeterScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.peeterScore.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.peeterScore.Location = new System.Drawing.Point(282, 153);
+            this.peeterScore.Name = "peeterScore";
+            this.peeterScore.Size = new System.Drawing.Size(146, 34);
+            this.peeterScore.TabIndex = 5;
+            this.peeterScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblJukuRand1
+            // 
+            this.lblJukuRand1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblJukuRand1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJukuRand1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblJukuRand1.Location = new System.Drawing.Point(52, 208);
+            this.lblJukuRand1.Name = "lblJukuRand1";
+            this.lblJukuRand1.Size = new System.Drawing.Size(44, 36);
+            this.lblJukuRand1.TabIndex = 6;
+            this.lblJukuRand1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblJukuRand2
+            // 
+            this.lblJukuRand2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblJukuRand2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJukuRand2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblJukuRand2.Location = new System.Drawing.Point(124, 208);
+            this.lblJukuRand2.Name = "lblJukuRand2";
+            this.lblJukuRand2.Size = new System.Drawing.Size(44, 36);
+            this.lblJukuRand2.TabIndex = 7;
+            this.lblJukuRand2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPeeterRand2
+            // 
+            this.lblPeeterRand2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblPeeterRand2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPeeterRand2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblPeeterRand2.Location = new System.Drawing.Point(371, 208);
+            this.lblPeeterRand2.Name = "lblPeeterRand2";
+            this.lblPeeterRand2.Size = new System.Drawing.Size(44, 36);
+            this.lblPeeterRand2.TabIndex = 9;
+            this.lblPeeterRand2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPeeterRand1
+            // 
+            this.lblPeeterRand1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblPeeterRand1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPeeterRand1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblPeeterRand1.Location = new System.Drawing.Point(299, 208);
+            this.lblPeeterRand1.Name = "lblPeeterRand1";
+            this.lblPeeterRand1.Size = new System.Drawing.Size(44, 36);
+            this.lblPeeterRand1.TabIndex = 8;
+            this.lblPeeterRand1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // playJuku
+            // 
+            this.playJuku.BackColor = System.Drawing.Color.RoyalBlue;
+            this.playJuku.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.playJuku.Enabled = false;
+            this.playJuku.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playJuku.Location = new System.Drawing.Point(52, 269);
+            this.playJuku.Name = "playJuku";
+            this.playJuku.Size = new System.Drawing.Size(116, 23);
+            this.playJuku.TabIndex = 10;
+            this.playJuku.Text = "Mängib Juku";
+            this.playJuku.UseVisualStyleBackColor = false;
+            this.playJuku.Click += new System.EventHandler(this.playJuku_Click);
+            // 
+            // playPeeter
+            // 
+            this.playPeeter.BackColor = System.Drawing.Color.RoyalBlue;
+            this.playPeeter.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.playPeeter.Enabled = false;
+            this.playPeeter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playPeeter.Location = new System.Drawing.Point(299, 269);
+            this.playPeeter.Name = "playPeeter";
+            this.playPeeter.Size = new System.Drawing.Size(116, 23);
+            this.playPeeter.TabIndex = 11;
+            this.playPeeter.Text = "Mängib Peeter";
+            this.playPeeter.UseVisualStyleBackColor = false;
+            this.playPeeter.Click += new System.EventHandler(this.playPeeter_Click);
+            // 
+            // btnStartNew
+            // 
+            this.btnStartNew.BackColor = System.Drawing.Color.OliveDrab;
+            this.btnStartNew.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnStartNew.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStartNew.ForeColor = System.Drawing.Color.White;
+            this.btnStartNew.Location = new System.Drawing.Point(177, 337);
+            this.btnStartNew.Name = "btnStartNew";
+            this.btnStartNew.Size = new System.Drawing.Size(116, 27);
+            this.btnStartNew.TabIndex = 12;
+            this.btnStartNew.Text = "Alusta uut mängu";
+            this.btnStartNew.UseVisualStyleBackColor = false;
+            this.btnStartNew.Click += new System.EventHandler(this.btnStartNew_Click);
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label9.Location = new System.Drawing.Point(33, 390);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(94, 52);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Tulemus";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTulemus
+            // 
+            this.lblTulemus.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblTulemus.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTulemus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTulemus.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblTulemus.Location = new System.Drawing.Point(143, 399);
+            this.lblTulemus.Name = "lblTulemus";
+            this.lblTulemus.Size = new System.Drawing.Size(190, 34);
+            this.lblTulemus.TabIndex = 14;
+            this.lblTulemus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 483);
+            this.Controls.Add(this.lblTulemus);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.btnStartNew);
+            this.Controls.Add(this.playPeeter);
+            this.Controls.Add(this.playJuku);
+            this.Controls.Add(this.lblPeeterRand2);
+            this.Controls.Add(this.lblPeeterRand1);
+            this.Controls.Add(this.lblJukuRand2);
+            this.Controls.Add(this.lblJukuRand1);
+            this.Controls.Add(this.peeterScore);
+            this.Controls.Add(this.jukuScore);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label jukuScore;
+        private System.Windows.Forms.Label peeterScore;
+        private System.Windows.Forms.Label lblJukuRand1;
+        private System.Windows.Forms.Label lblJukuRand2;
+        private System.Windows.Forms.Label lblPeeterRand2;
+        private System.Windows.Forms.Label lblPeeterRand1;
+        private System.Windows.Forms.Button playJuku;
+        private System.Windows.Forms.Button playPeeter;
+        private System.Windows.Forms.Button btnStartNew;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblTulemus;
+    }
+}
+
---
+++ piil_rainer_täring/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace piil_rainer_täring
+{
+    public partial class Form1 : Form
+    {
+        public int juku;
+        public int peeter;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnStartNew_Click(object sender, EventArgs e)
+        {
+            jukuScore.Text = "";
+            lblJukuRand1.Text = "";
+            lblJukuRand2.Text = "";
+            peeterScore.Text = "";
+            lblPeeterRand1.Text = "";
+            lblPeeterRand2.Text = "";
+            lblTulemus.Text = "";
+            btnStartNew.Enabled = false;
+            playJuku.Enabled = true;
+        }
+        private void playJuku_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int random1 = rnd.Next(1, 7);
+            int random2 = rnd.Next(1, 7);
+            lblJukuRand1.Text = random1.ToString();
+            lblJukuRand2.Text = random2.ToString();
+            jukuScore.Text = (random1 + random2).ToString();
+            playJuku.Enabled = false;
+            playPeeter.Enabled = true;
+        }
+
+        private void playPeeter_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int random1 = rnd.Next(1, 7);
+            int random2 = rnd.Next(1, 7);
+            lblPeeterRand1.Text = random1.ToString();
+            lblPeeterRand2.Text = random2.ToString();
+            peeterScore.Text = (random1 + random2).ToString();
+            btnStartNew.Enabled = true;
+            playPeeter.Enabled = false;
+            juku = Convert.ToInt32(jukuScore.Text);
+            peeter = Convert.ToInt32(peeterScore.Text);
+            if (juku > peeter)
+            {
+                lblTulemus.Text = "Võitis Juku";
+            }
+            else if (peeter > juku)
+            {
+                lblTulemus.Text = "Võitis Peeter";
+
+            }
+            else
+            {
+                lblTulemus.Text = "Viik";
+
+            }
+
+
+        }
+
+
+    }
+}
